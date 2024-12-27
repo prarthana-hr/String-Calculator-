24f81842abddbcc0eb9236bc7909c5f292783565
Prarthana <prarthanahrrao@gmail.com>
2024-12-27 18:42:30 +0530

    Add method to handle an unknown amount of numbers

--- String_Calculator/src/test/java/org/exampleTest/StringCalculatorTest.java
+++ String_Calculator/src/test/java/org/exampleTest/StringCalculatorTest.java
@@ -6,11 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
     @Test
-    public void testAddEmptyString(){
-        StringCalculator stringCalculator=new StringCalculator();
-        assertEquals(3, stringCalculator.add("1,2"));
-        assertEquals(10, stringCalculator.add("7,3"));
+    public void testAddEmptyString() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add("")); // Empty string should return 0
+        assertEquals(1, stringCalculator.add("1"));// Single number should return the number itself
+        assertEquals(3, stringCalculator.add("1,2"));// Two numbers should return their sum
+        assertEquals(6, stringCalculator.add("1,2,3"));// Sum of 1, 2, 3 is 6
+        assertEquals(15, stringCalculator.add("1,2,3,4,5"));// Sum of 1, 2, 3, 4, 5 is 15
+        assertEquals(0, stringCalculator.add("0,0,0"));// Sum of 0, 0, 0 is 0
     }
-
-
 }
